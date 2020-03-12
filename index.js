834e70ffa944bc55f8c92ad5d2b28e3a79bae7f4
github username <github email address>
2020-03-12 17:03:09 -0400

    deliverables delivered

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 /***** Deliverable 1 *****/
 const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+const playerContainer = document.querySelector('.player-container')
 
 
 /***** Deliverable 2 *****/
+header.style.color = 'red'
 
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+PLAYERS.forEach(player => playerProfile(player))
+
+function playerProfile(player){
+    let playerDiv = document.createElement('div')
+    let playerH3 = document.createElement('h3')
+    let playerImage = document.createElement('img')
+    playerContainer.appendChild(playerDiv)
+    playerDiv.appendChild(playerH3)
+    playerDiv.appendChild(playerImage)
+
+    playerDiv.className = 'player'
+    playerDiv.dataset.number = player.number
+    playerH3.innerHTML = `${player.name} (<em>${player.nickname}</em>)`
+    playerImage.src = player.photo
+    playerImage.alt = player.name
+}
 
 
 /***** Deliverable 4 *****/
+document.querySelector('[data-number="7"]').remove()
